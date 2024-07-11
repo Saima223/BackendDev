7de85da99c2bcd1f22761cff3b44f0b490751a31
Saima223 <siddiqui22062004@gmail.com>
2024-07-12 01:28:19 +0530

    Going to deploye my 1st app

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -54,6 +54,6 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 })
