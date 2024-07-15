4155c6e1e881d9711b723c05c1cdb29cf819696c
Saima223 <siddiqui22062004@gmail.com>
2024-07-15 19:42:33 +0530

    Passing data from webpage to server and add the style.css file, learn and implemente the scope.

--- Todo-list/app.js
+++ Todo-list/app.js
@@ -2,38 +2,42 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.get("/", function (req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-            default:
-            console.log("Error: current day is equal to: " + currentDay);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function(req, res){
+
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
     }
-    res.render("list", { kindOfDay: day });
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.listen(3000, function () {
